8342f8b722f12476fbc6dd40ca0fcfb5f6e88b97
Alexey <alexey.lazarenko@jelastic.com>
2014-09-29 09:19:05 +0300

    credentials

--- versions/0.8.7/conf/mysql_vmail.sql
+++ versions/0.8.7/conf/mysql_vmail.sql
@@ -2,20 +2,20 @@
 CREATE DATABASE IF NOT EXISTS vmail CHARACTER SET utf8;
 
 /* Permissions. */
-GRANT SELECT ON vmail.* TO "vmail"@"localhost" IDENTIFIED BY "OeRroX165V0pWElJlNl6PPvK2jbgZU";
-GRANT SELECT,INSERT,DELETE,UPDATE ON vmail.* TO "vmailadmin"@"localhost" IDENTIFIED BY "abtQOhMX0WNuuw9QtqtJUuI7nVVnXi";
+GRANT SELECT ON vmail.* TO "vmail"@"localhost" IDENTIFIED BY "H8rcg9QrlnsbXLd5HyDm2UgDwhKzel";
+GRANT SELECT,INSERT,DELETE,UPDATE ON vmail.* TO "vmailadmin"@"localhost" IDENTIFIED BY "IOaUUSBXg43Dk8GMTWnO0J2jVXJOkh";
 
 /* Initialize the database. */
 USE vmail;
-SOURCE /opt/repo/versions/0.8.7/samples/iredmail.mysql;
+SOURCE /opt/repo/samples/iredmail.mysql;
 
 /* Add your first domain. */
-INSERT INTO domain (domain,transport,created) VALUES ("iredmail.dev.jelastic.com", "dovecot", NOW());
+INSERT INTO domain (domain,transport,created) VALUES ("example.com", "dovecot", NOW());
 
 /* Add your first normal user. */
-INSERT INTO mailbox (username,password,name,maildir,quota,domain,isadmin,isglobaladmin,created) VALUES ("postmaster@iredmail.example.com","$1$osgTHTDY$coMiQrobEFSCmdlu0V2Om1","postmaster","iredmail.example.com/p/o/s/postmaster-2014.09.19.13.15.37/",100, "iredmail.example.com", 1, 1, NOW());
-INSERT INTO alias (address,goto,domain,created) VALUES ("postmaster@iredmail.example.com", "postmaster@iredmail.example.com", "iredmail.example.com", NOW());
+INSERT INTO mailbox (username,password,name,maildir,quota,domain,isadmin,isglobaladmin,created) VALUES ("postmaster@example.com","$1$Rvv0jcpZ$4kHlqs9.4.P/ta2aThq0o0","postmaster","example.com/p/o/s/postmaster-2014.09.26.16.29.40/",100, "example.com", 1, 1, NOW());
+INSERT INTO alias (address,goto,domain,created) VALUES ("postmaster@example.com", "postmaster@example.com", "example.com", NOW());
 
 /* Mark first mail user as global admin */
-INSERT INTO domain_admins (username,domain,created) VALUES ("postmaster@iredmail.example.com","ALL", NOW());
+INSERT INTO domain_admins (username,domain,created) VALUES ("postmaster@example.com","ALL", NOW());
 
--- versions/0.8.7/pkgs/get_all.sh
+++ versions/0.8.7/pkgs/get_all.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
-# Author:   Zhang Huangbin (zhb _at_ iredmail.org)
-# Purpose:  Fetch all extra packages we need to build mail server.
-
+# Author: Zhang Huangbin (zhb _at_ iredmail.org)
+# Purpose: Fetch all extra packages we need to build mail server.
 #---------------------------------------------------------------------
 # This file is part of iRedMail, which is an open source mail server
 # solution for Red Hat(R) Enterprise Linux, CentOS, Debian and Ubuntu.
@@ -14,157 +12,131 @@
 #
 # iRedMail is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with iRedMail.  If not, see <http://www.gnu.org/licenses/>.
+# along with iRedMail. If not, see <http://www.gnu.org/licenses/>.
 #---------------------------------------------------------------------
-
 ROOTDIR="$(pwd)"
 CONF_DIR="${ROOTDIR}/../conf"
-
 . ${CONF_DIR}/global
 . ${CONF_DIR}/core
 . ${CONF_DIR}/iredadmin
-
 # Re-define @STATUS_FILE, so that iRedMail.sh can read it.
 export STATUS_FILE="${ROOTDIR}/../.status"
-
 check_user root
 check_hostname
-
 # Where to fetch/store binary packages and source tarball.
 export IREDMAIL_MIRROR="${IREDMAIL_MIRROR:=http://iredmail.org}"
 export PKG_DIR="${ROOTDIR}/pkgs"
 export MISC_DIR="${ROOTDIR}/misc"
-
 if [ X"${DISTRO}" == X"RHEL" ]; then
-    # Special package.
-    # command: which.
-    export BIN_WHICH='which'
-    export PKG_WHICH="which${PKG_ARCH}"
-    # command: wget.
-    export BIN_WGET='wget'
-    export PKG_WGET="wget${PKG_ARCH}"
-
+# Special package.
+# command: which.
+export BIN_WHICH='which'
+export PKG_WHICH="which${PKG_ARCH}"
+# command: wget.
+export BIN_WGET='wget'
+export PKG_WGET="wget${PKG_ARCH}"
 elif [ X"${DISTRO}" == X"DEBIAN" -o X"${DISTRO}" == X"UBUNTU" ]; then
-    if [ X"${OS_ARCH}" == X"x86_64" ]; then
-        export pkg_arch='amd64'
-    else
-        export pkg_arch="${OS_ARCH}"
-    fi
-
-    # Special package.
-    # command: which.
-    export BIN_WHICH='which'
-    export PKG_WHICH="debianutils"
-    # command: wget.
-    export BIN_WGET='wget'
-    export PKG_WGET="wget"
-    # command: dpkg-scanpackages.
-    export BIN_CREATEREPO="dpkg-scanpackages"
-    export PKG_CREATEREPO="dpkg-dev"
+if [ X"${OS_ARCH}" == X"x86_64" ]; then
+export pkg_arch='amd64'
+else
+export pkg_arch="${OS_ARCH}"
 fi
-
+# Special package.
+# command: which.
+export BIN_WHICH='which'
+export PKG_WHICH="debianutils"
+# command: wget.
+export BIN_WGET='wget'
+export PKG_WGET="wget"
+# command: dpkg-scanpackages.
+export BIN_CREATEREPO="dpkg-scanpackages"
+export PKG_CREATEREPO="dpkg-dev"
+fi
 # Binary packages.
 export pkg_total=$(echo ${PKGLIST} | wc -w | awk '{print $1}')
 export pkg_counter=1
-
 # Misc file (source tarball) list.
 if [ X"${DISTRO}" == X"FREEBSD" ]; then
-    PKGMISC='SHASUM.freebsd.misc'
+PKGMISC='SHASUM.freebsd.misc'
 elif [ X"${DISTRO}" == X'OPENBSD' ]; then
-    PKGMISC='MD5.openbsd'
+PKGMISC='MD5.openbsd'
 else
-    PKGMISC='MD5.misc'
+PKGMISC='MD5.misc'
 fi
 MISCLIST="$(cat ${ROOTDIR}/${PKGMISC} | awk -F'misc/' '{print $2}')"
-
 prepare_dirs()
 {
-    ECHO_DEBUG "Creating necessary directories ..."
-    for i in ${PKG_DIR} ${MISC_DIR}
-    do
-        [ -d "${i}" ] || mkdir -p "${i}"
-    done
+ECHO_DEBUG "Creating necessary directories ..."
+for i in ${PKG_DIR} ${MISC_DIR}
+do
+[ -d "${i}" ] || mkdir -p "${i}"
+done
 }
-
 fetch_misc()
 {
-    # Fetch all misc packages.
-    cd ${MISC_DIR}
-
-    misc_total=$(( $(echo ${MISCLIST} | wc -w | awk '{print $1}') ))
-    misc_count=1
-
-    ECHO_INFO "Fetching source tarballs ..."
-
-    for i in ${MISCLIST}; do
-        url="${IREDMAIL_MIRROR}/yum/misc/${i}"
-        ECHO_INFO "+ ${misc_count} of ${misc_total}: ${url}"
-
-        ${FETCH_CMD} "${url}"
-
-        misc_count=$((misc_count + 1))
-    done
+# Fetch all misc packages.
+cd ${MISC_DIR}
+misc_total=$(( $(echo ${MISCLIST} | wc -w | awk '{print $1}') ))
+misc_count=1
+ECHO_INFO "Fetching source tarballs ..."
+for i in ${MISCLIST}; do
+url="${IREDMAIL_MIRROR}/yum/misc/${i}"
+ECHO_INFO "+ ${misc_count} of ${misc_total}: ${url}"
+${FETCH_CMD} "${url}"
+misc_count=$((misc_count + 1))
+done
 }
-
 check_md5()
 {
-    cd ${ROOTDIR}
-
-    if [ X"${DISTRO}" != X"FREEBSD" ]; then
-        ECHO_INFO -n "Validate packages ..."
-
-        md5file="/tmp/check_md5_tmp.${RANDOM}$RANDOM}"
-        echo -e "${MD5LIST}" > ${md5file}
-        cat ${PKGMISC} >> ${md5file}
-        if [ X"${DISTRO}" == X'OPENBSD' ]; then
-            md5 -c ${md5file} |grep 'FAILED'
-            RETVAL="$?"
-        else
-            md5sum -c ${md5file} |grep 'FAILED'
-            RETVAL="$?"
-        fi
-        rm -f ${md5file} 2>/dev/null
-
-        if [ X"${RETVAL}" == X"0" ]; then
-            echo -e "\t[ FAILED ]"
-            ECHO_ERROR "MD5 check failed. Script exit ...\n"
-            return 0
-            #exit 255
-        else
-            echo -e "\t[ OK ]"
-            echo 'export status_fetch_misc="DONE"' >> ${STATUS_FILE}
-            echo 'export status_check_md5="DONE"' >> ${STATUS_FILE}
-        fi
-    fi
+cd ${ROOTDIR}
+if [ X"${DISTRO}" != X"FREEBSD" ]; then
+ECHO_INFO -n "Validate packages ..."
+md5file="/tmp/check_md5_tmp.${RANDOM}$RANDOM}"
+echo -e "${MD5LIST}" > ${md5file}
+cat ${PKGMISC} >> ${md5file}
+if [ X"${DISTRO}" == X'OPENBSD' ]; then
+md5 -c ${md5file} |grep 'FAILED'
+RETVAL="$?"
+else
+md5sum -c ${md5file} |grep 'FAILED'
+RETVAL="$?"
+fi
+rm -f ${md5file} 2>/dev/null
+if [ X"${RETVAL}" == X"0" ]; then
+echo -e "\t[ FAILED ]"
+ECHO_ERROR "MD5 check failed. Script exit ...\n"
+return 0
+#exit 255
+else
+echo -e "\t[ OK ]"
+echo 'export status_fetch_misc="DONE"' >> ${STATUS_FILE}
+echo 'export status_check_md5="DONE"' >> ${STATUS_FILE}
+fi
+fi
 }
-
 create_repo_rhel()
 {
-    # createrepo
-    ECHO_INFO "Generating yum repository ..."
-
-    # Backup old repo file.
-    backup_file ${LOCAL_REPO_FILE}
-
-    # Generate new repo file.
-    cat > ${LOCAL_REPO_FILE} <<EOF
+# createrepo
+ECHO_INFO "Generating yum repository ..."
+# Backup old repo file.
+backup_file ${LOCAL_REPO_FILE}
+# Generate new repo file.
+cat > ${LOCAL_REPO_FILE} <<EOF
 [${LOCAL_REPO_NAME}]
 name=${LOCAL_REPO_NAME}
 baseurl=${IREDMAIL_MIRROR}/yum/rpms/${DISTRO_VERSION}/
 enabled=1
 gpgcheck=0
 EOF
-
-    ECHO_INFO "Clean metadata of yum repositories."
-    yum clean metadata
-
-    # RHEL/CentOS 6.
-    # Create a temporary yum repo to install epel-release without GPG check.
-    cat > ${YUM_REPOS_DIR}/tmp_epel.repo <<EOF
+ECHO_INFO "Clean metadata of yum repositories."
+yum clean metadata
+# RHEL/CentOS 6.
+# Create a temporary yum repo to install epel-release without GPG check.
+cat > ${YUM_REPOS_DIR}/tmp_epel.repo <<EOF
 [tmp_epel]
 name=Extra Packages for Enterprise Linux ${DISTRO_VERSION} - \$basearch
 #baseurl=http://download.fedoraproject.org/pub/epel/${DISTRO_VERSION}/\$basearch
@@ -173,87 +145,71 @@ failovermethod=priority
 enabled=1
 gpgcheck=0
 EOF
-
-    ECHO_INFO "Install epel yum repo."
-    eval ${install_pkg} epel-release && rm ${YUM_REPOS_DIR}/tmp_epel.repo
-    yum clean metadata
-
-    echo 'export status_create_repo_rhel="DONE"' >> ${STATUS_FILE}
+ECHO_INFO "Install epel yum repo."
+eval ${install_pkg} epel-release && rm ${YUM_REPOS_DIR}/tmp_epel.repo
+yum clean metadata
+echo 'export status_create_repo_rhel="DONE"' >> ${STATUS_FILE}
 }
-
 check_new_iredmail()
 {
-    # Check new version and track basic information,
-    # Used to help iRedMail team understand which Linux/BSD distribution
-    # we should take more care of.
-    # iRedMail version number, OS distribution, release version, code name, backend.
-    ECHO_INFO "Checking new version of iRedMail ..."
-    ${FETCH_CMD} "${IREDMAIL_MIRROR}/version/check.py/iredmail_os?iredmail_version=${PROG_VERSION}&arch=${OS_ARCH}&distro=${DISTRO}&distro_version=${DISTRO_VERSION}&distro_code_name=${DISTRO_CODENAME}" &>/dev/null
-
-    UPDATE_AVAILABLE='NO'
-    if ls iredmail_os* &>/dev/null; then
-        info="$(cat iredmail_os*)"
-        if [ X"${info}" == X'UPDATE_AVAILABLE' ]; then
-            UPDATE_AVAILABLE='YES'
-        fi
-    fi
-
-    rm -f iredmail_os* &>/dev/null
-
-    if [ X"${UPDATE_AVAILABLE}" == X'YES' ]; then
-        echo ''
-        ECHO_ERROR "Your iRedMail version (${PROG_VERSION}) is out of date, please"
-        ECHO_ERROR "download the latest version and try again:"
-        ECHO_ERROR "http://www.iredmail.org/download.html"
-        echo ''
-        exit 255
-    fi
-
-    echo 'export status_check_new_iredmail="DONE"' >> ${STATUS_FILE}
+# Check new version and track basic information,
+# Used to help iRedMail team understand which Linux/BSD distribution
+# we should take more care of.
+# iRedMail version number, OS distribution, release version, code name, backend.
+ECHO_INFO "Checking new version of iRedMail ..."
+${FETCH_CMD} "${IREDMAIL_MIRROR}/version/check.py/iredmail_os?iredmail_version=${PROG_VERSION}&arch=${OS_ARCH}&distro=${DISTRO}&distro_version=${DISTRO_VERSION}&distro_code_name=${DISTRO_CODENAME}" &>/dev/null
+UPDATE_AVAILABLE='NO'
+if ls iredmail_os* &>/dev/null; then
+info="$(cat iredmail_os*)"
+if [ X"${info}" == X'UPDATE_AVAILABLE' ]; then
+UPDATE_AVAILABLE='YES'
+fi
+fi
+rm -f iredmail_os* &>/dev/null
+if [ X"${UPDATE_AVAILABLE}" == X'YES' ]; then
+echo ''
+ECHO_ERROR "Your iRedMail version (${PROG_VERSION}) is out of date, please"
+ECHO_ERROR "download the latest version and try again:"
+ECHO_ERROR "http://www.iredmail.org/download.html"
+echo ''
+exit 255
+fi
+echo 'export status_check_new_iredmail="DONE"' >> ${STATUS_FILE}
 }
-
 echo_end_msg()
 {
-    if [ X"$(basename $0)" != X'get_all.sh' ]; then
-        cat <<EOF
+if [ X"$(basename $0)" != X'get_all.sh' ]; then
+cat <<EOF
 ********************************************************
 * All tasks had been finished successfully. Next step:
 *
-*   # cd ..
-*   # bash ${PROG_NAME}.sh
+* # cd ..
+* # bash ${PROG_NAME}.sh
 *
 ********************************************************
-
 EOF
-    fi
-}
-
-if [ -e ${STATUS_FILE} ]; then
-    . ${STATUS_FILE}
-else
-    echo '' > ${STATUS_FILE}
 fi
-
+}
+if [ -e ${STATUS_FILE} ]; then
+. ${STATUS_FILE}
+else
+echo '' > ${STATUS_FILE}
+fi
 # Check latest version
 [ X"${CHECK_NEW_IREDMAIL}" != X'NO' ] && \
-    check_status_before_run check_new_iredmail
-
+check_status_before_run check_new_iredmail
 prepare_dirs
-
 if [ X"${DISTRO}" == X"RHEL" ]; then
-    # Create yum repository.
-    check_status_before_run create_repo_rhel
-
-    # Check required commands, install related package if command doesn't exist.
-    check_pkg ${BIN_WHICH} ${PKG_WHICH}
-    check_pkg ${BIN_WGET} ${PKG_WGET}
-
+# Create yum repository.
+check_status_before_run create_repo_rhel
+# Check required commands, install related package if command doesn't exist.
+check_pkg ${BIN_WHICH} ${PKG_WHICH}
+check_pkg ${BIN_WGET} ${PKG_WGET}
 elif [ X"${DISTRO}" == X'DEBIAN' -o X"${DISTRO}" == X'UBUNTU' ]; then
-    # Force update.
-    ECHO_INFO "Resynchronizing the package index files (apt-get update) ..."
-    ${APTGET} update
+# Force update.
+ECHO_INFO "Resynchronizing the package index files (apt-get update) ..."
+${APTGET} update
 fi
-
 check_status_before_run fetch_misc && \
 check_status_before_run check_md5 && \
 check_pkg ${BIN_DIALOG} ${PKG_DIALOG} && \
